59fc3658ef82a34e12b86696b372e9e2e5ec317d
Rohit Kumar Ranjan <119317461+Rohitkrranjan@users.noreply.github.com>
2024-02-13 23:11:40 +0530

    Add files via upload

---
+++ DataType/src/Escape.java
@@ -0,0 +1,10 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Hello \"Rohit\"...");
+        System.out.println("Hello \'Rohit\'...");
+        System.out.println("Hello \\Rohit\\...");
+        System.out.println("Hello \nRohit...");
+        System.out.println("Hello \tRohit...");
+        System.out.println("Hello\bRohit...");
+    }
+}
---
+++ DataType/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args)
+    {
+      // integer data type
+        int myNumber=10;
+        System.out.println(myNumber);
+
+        // float data type
+        float yourNumber = 12;
+        System.out.println(yourNumber);
+        // double data type
+        double x = 12.34;
+        System.out.println(x);
+
+        // boolean data type
+        boolean y=true;
+        System.out.println((y));
+
+        // string
+        String name = "Rohit";
+        System.out.println(name);
+    }
+}
